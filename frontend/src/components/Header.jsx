f0604ec54681a577f9db355fa0ae4df271c77649
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-04-30 13:31:27 +0300

    adding font awesome to index and navbar on header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,27 @@
+import { Container, Navbar, Nav } from 'react-bootstrap';
 
 function Header() {
   return (
-    <div>
-      <header>Header</header>
-    </div>
+    <header>
+      <Navbar expand="lg" bg='dark' variant='dark' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">TenniShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <i className='fas fa-shopping-cart'></i>
+                Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className='fas fa-user'></i>
+                Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
 
