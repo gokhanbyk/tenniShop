eb920780f794b05374a1dea028d204cdc80442ac
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-04-30 19:21:43 +0300

    implementing router to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen.jsx";
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // React Bootstrap
 import { Container } from 'react-bootstrap';
@@ -9,15 +10,17 @@ import { Container } from 'react-bootstrap';
 function App() {
 
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" Component={HomeScreen} exact />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
