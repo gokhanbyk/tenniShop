9ced605459baca4d97580ce2415ad93d4e485130
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-05-01 11:27:33 +0300

    getting api's from restframework

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', getRoutes, name='routes'),
     path('products/', getProducts, name='products'),
+    path('products/<str:pk>', getProduct, name='product'),
 ]
