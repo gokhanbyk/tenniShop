78f5e8f34c54f23b4852c095e17ceaf7a5bafaf9
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-05-01 12:57:08 +0300

    adding media and static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -127,6 +127,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_ROOT = 'static/images'
+
+MEDIA_URL = '/images/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
