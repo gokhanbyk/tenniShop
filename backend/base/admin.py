9e88b26e5c530717e8fcc78e5127c1ee46b85371
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-05-01 12:50:40 +0300

    Database models

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -3,3 +3,7 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
