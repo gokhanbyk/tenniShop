83fd0b1ca7efc29d8b2a2c02931398436db730ed
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-05-01 16:10:44 +0300

    message's variant changed to warning

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -20,7 +20,7 @@ function HomeScreen() {
     <div>
       <h1>Latest Products</h1>
       {loading ? <Loader />
-        : error ? <Message variant='danger'>{error}</Message>
+        : error ? <Message variant='warning'>{error}</Message>
           : <Row>
             {products.map(product => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
