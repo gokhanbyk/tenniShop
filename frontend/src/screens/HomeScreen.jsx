1845bc1e14da91037851bb45064b3785a79ff6d5
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-05-01 12:08:05 +0300

    baseUrl for axios

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -7,9 +7,11 @@ import { useState, useEffect } from 'react';
 function HomeScreen() {
   const [products, setProducts] = useState([]);
 
+  axios.defaults.baseURL = 'http://127.0.0.1:8000';
+
   useEffect(() => {
     async function fetchProducts() {
-      const { data } = await axios.get('http://127.0.0.1:8000/api/products/');
+      const { data } = await axios.get('/api/products/');
       setProducts(data);
     }
 
