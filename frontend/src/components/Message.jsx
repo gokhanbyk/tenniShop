2fe5d5f905220dfb764dd8ed96d756f70e45f8a5
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-05-01 16:08:59 +0300

    message component added to homescreen

---
+++ frontend/src/components/Message.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import { Alert } from 'react-bootstrap';
+
+function Message({ variant, children }) {
+  return (
+    <Alert variant={variant}>
+      {children}
+    </Alert>
+  );
+}
+
+export default Message;
