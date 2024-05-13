883d2782bc2d5cec8723d62de9acbd5d011e5dd8
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-05-13 20:51:44 +0300

    customize the validation from simplejwt

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from .views import *
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
+
 
 
 urlpatterns = [
-    path('users/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', getRoutes, name='routes'),
     path('products/', getProducts, name='products'),
     path('products/<str:pk>', getProduct, name='product'),
--- backend/base/views.py
+++ backend/base/views.py
@@ -8,6 +8,22 @@ from .serializers import ProductSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+# simplejwt
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+   serializer_class = MyTokenObtainPairSerializer
+
 # Create your views here.
 
 @api_view(['GET'])
