2323af9da7e7e9fc5f81b571e85794c03fdf60ee
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-05-15 14:40:06 +0300

    checkout steps componenet

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { Form, Button, } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer.jsx';
+import CheckoutSteps from '../components/CheckoutSteps.jsx';
 import { saveShippingAddress } from '../actions/cartActions.jsx';
 
 function ShippingScreen() {
@@ -32,6 +33,7 @@ function ShippingScreen() {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
 
