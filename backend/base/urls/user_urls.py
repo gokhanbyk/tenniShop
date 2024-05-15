6d29df31e069e074342852dcd27340bca4e1a37a
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-05-15 09:46:13 +0300

    update profile endpoint

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', getUsers, name='users'),
     
     path('profile/', getUserProfile, name='users-profile'),
+    path('profile/update/', updateUserProfile, name='user-profile-update'),
     
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', registerUser, name='register'),
