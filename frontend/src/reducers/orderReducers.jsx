7903259cec8fdc33477eb6e05cf6a0051d55c6bc
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-07-23 12:20:45 +0300

    order pay reducer and action

--- frontend/src/reducers/orderReducers.jsx
+++ frontend/src/reducers/orderReducers.jsx
@@ -7,6 +7,11 @@ import {
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
   ORDER_DETAILS_FAIL,
+
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_FAIL,
+  ORDER_PAY_RESET,
 } from '../constants/orderConstants';
 
 export const orderDetailsReducer = (state = { loading: true, orderItems: [], shippingAddress: {} }, action) => {
@@ -53,6 +58,31 @@ export const orderCreateReducer = (state = {}, action) => {
     case ORDER_CREATE_RESET:
       return {};
 
+    default:
+      return state;
+  }
+};
+
+export const orderPayReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ORDER_PAY_REQUEST:
+      return {
+        loading: true
+      };
+
+    case ORDER_PAY_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+      };
+
+    case ORDER_PAY_FAIL:
+      return {
+        loading: false,
+        error: action.payload
+      };
+    case ORDER_PAY_RESET:
+      return {};
     default:
       return state;
   }
--- frontend/src/store.jsx
+++ frontend/src/store.jsx
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducers, productDetailsReducers } from './reducers/productReducers.jsx';
 import { cartReducer } from './reducers/cartReducers.jsx';
 import { userLoginReducers, userRegisterReducers, userDetailsReducers, userUpdateProfileReducers } from './reducers/userReducers.jsx';
-import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers.jsx';
+import { orderCreateReducer, orderDetailsReducer, orderPayReducer } from './reducers/orderReducers.jsx';
 
 const reducer = combineReducers({
   productList: productListReducers,
@@ -16,6 +16,7 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducers,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
