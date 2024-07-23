1058d923e2c73fa91512ae46b0d66d9490a82930
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-07-23 13:48:43 +0300

    my orders for backend

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -5,6 +5,8 @@ from base.views.order_views import *
 
 urlpatterns = [
 path('add/', addOrderItems, name='orders-add'),
+path('my-orders/', getMyOrders, name='my-orders'),
+
 path('<str:pk>/', getOrderById, name='user-order'),
 path('<str:pk>/pay/', updateOrderToPaid, name='pay'),
 ]
